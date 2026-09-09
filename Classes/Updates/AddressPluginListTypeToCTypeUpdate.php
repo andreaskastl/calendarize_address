36dc28116676b79b238fab00d496c831a5be6e53
Sybille Peters <sypets@gmx.de>
2026-09-09 15:19:12 +0200

    [BUGFIX] Fix upgrade wizard for v13
    AddressPluginListTypeToCTypeUpdate upgrade wizard is fixed for v13 by using the
    namespace for v13 for the class AbstractListTypeToCTypeUpdate and Attribute\UpgradeWizard.
    
    This will also resolve in v14 due to aliases in the core.
    
    Resolves: #15

--- Classes/Updates/AddressPluginListTypeToCTypeUpdate.php
+++ Classes/Updates/AddressPluginListTypeToCTypeUpdate.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace AndreasKastl\CalendarizeAddress\Updates;
 
-use TYPO3\CMS\Core\Attribute\UpgradeWizard;
-use TYPO3\CMS\Core\Upgrades\AbstractListTypeToCTypeUpdate;
+// v13 path, works for v13 and v14
+use TYPO3\CMS\Install\Attribute\UpgradeWizard;
+
+// v14 namespace
+// use TYPO3\CMS\Core\Attribute\UpgradeWizard;
+
+// use TYPO3 v13 namespace here to be compatible with v13, otherwise upgrade wizard will not work in v13 !
+use TYPO3\CMS\Install\Updates\AbstractListTypeToCTypeUpdate;
+
+// TYPO3 v14 namespace
+// use TYPO3\CMS\Core\Upgrades\AbstractListTypeToCTypeUpdate;
 
 #[UpgradeWizard('calendarize_address_pluginListTypeToCTypeUpdate')]
 class AddressPluginListTypeToCTypeUpdate extends AbstractListTypeToCTypeUpdate
